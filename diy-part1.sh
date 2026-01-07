aed0f97ee0d3079ddc377eb21fa46c8cd219d7ad
wsh19770504 <86936359+wsh19770504@users.noreply.github.com>
2026-01-07 22:14:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,9 +5,9 @@
 
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
-#sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' ./feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' ./feeds.conf.default
 
 # homeproxy
 #git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
