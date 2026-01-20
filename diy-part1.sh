64a69c023b3affd40eeda886be94ef467699e326
wsh19770504 <86936359+wsh19770504@users.noreply.github.com>
2026-01-20 16:43:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' ./feeds.conf.default
 
 # homeproxy
