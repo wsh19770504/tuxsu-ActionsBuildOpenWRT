af7f6d962f38ed0047901cba761bb9635bee2ada
wsh19770504 <86936359+wsh19770504@users.noreply.github.com>
2026-01-20 16:44:54 +0800

    Update x86_64-part2.sh

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -2,5 +2,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/luci/bin/config_generate
